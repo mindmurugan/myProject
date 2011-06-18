81830d6364257970bb0018edecf2a63e5c1ea82c
Murugan Palaniappan <mindmurugan@yahoo.com>
2011-06-18 08:38:30 +0800

    Added Templates folder
    Signed-off-by: Murugan Palaniappan <mindmurugan@yahoo.com>

---
+++ templates/template.hpp
@@ -0,0 +1,37 @@
+/*
+ * @CLASS@.hpp
+ * Copyright (C) @FIRSTNAME@ @LASTNAME@ @YEAR@ <@EMAIL@>
+ *
+ * @CLASS@.hpp is free software: you can redistribute it and/or modify it
+ * under the terms of thppe GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @CLASS@.hpp is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef @CLASS@_hpp
+#define @CLASS@_hpp
+
+namespace @NAMESPACE@
+{
+
+class @CLASS@
+{
+public:
+    @CLASS@();
+    virtual ~@CLASS@();
+private:
+};
+
+} // @NAMESPACE@
+
+#endif
+
+// vim:ts=4:sw=4
